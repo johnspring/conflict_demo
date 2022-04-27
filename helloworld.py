3f06c5fc88fc6b691b84039fd20c8b4f69f2f8fb
John Spring <johnspring05@gmail.com>
2022-04-26 20:13:47 -0400

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(city,state):
-    print(f'Hello World!{city,state}')
+def hello_world(city, state):
+    print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World! {city}')
-
-
-def main():
-    hello_world('nyc')
->>>>>>> 0d9bb4dbfced71e59a7e83c7a6da7541bb44215e
 
 
 main()
